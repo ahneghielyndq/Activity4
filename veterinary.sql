4d32a1b54c4220c8988b7360bc7d76fd6e5289c9
biancacorpuz12 <biancacorpuz16@gmail.com>
2025-12-02 18:04:12 +0800

    feat/create-table medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -47,3 +47,14 @@ CREATE TABLE invoices (
 appointments(appointid)
 );
 
+CREATE TABLE medicalrecords ( 
+    recordid INT PRIMARY KEY, 
+    animalid INT, 
+    recorddate DATETIME, 
+    doctorid INT, 
+    diagnosis VARCHAR(255), 
+    prescription VARCHAR(255), 
+    notes TEXT, 
+    FOREIGN KEY (animalid) REFERENCES animals(animalid), 
+    FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
+);
